721144ea705f96bfeb024d6b77027f715f52f084
mason <masonlee7629@gmail.com>
2023-03-13 05:06:22 +0900

    [Feat] Num_10250 - solved

---
+++ src/solvedac/class_02/Num_10250/Main.java
@@ -0,0 +1,29 @@
+package solvedac.class_02.Num_10250;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+
+            int floor = (n % h == 0) ? h : (n % h);
+            int roomNumber = (n % h == 0) ? (n / h) : (n / h + 1);
+
+            sb.append(floor * 100 + roomNumber).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
